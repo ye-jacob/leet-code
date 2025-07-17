f1f567ed47779a74232321191533f404f5c5f94f
Jacob Ye <ye.jacob@gmail.com>
2025-07-16 21:57:03 -0400

    testing

--- 07-16-2025/problem.py
+++ 07-16-2025/problem.py
@@ -4,4 +4,14 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        
+        words = s.split()
+        print(words)
+        for i in range(len(words)):
+            words[i] = words[i][::-1]
+        return ' '.join(words)
+
+print(Solution().reverseWords("Let's take LeetCode contest"))
+
+
+
+
