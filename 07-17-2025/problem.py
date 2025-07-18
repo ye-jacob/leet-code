d3ed81f7021615a2b7a47b9cd0148551ec54fde8
Jacob Ye <ye.jacob@gmail.com>
2025-07-17 23:42:02 -0400

    new changes

--- 07-17-2025/problem.py
+++ 07-17-2025/problem.py
@@ -19,3 +19,11 @@ class Solution(object):
             val2 = l2.val if l2 else 0
 
             total = val1 + val2 + carry
+            carry = total // 10
+            current.next = ListNode(total % 10)
+            current = current.next
+
+            if l1: l1 = l1.next
+            if l2: l2 = l2.next
+
+        return dummy.next
