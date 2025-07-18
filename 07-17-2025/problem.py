9b9bc0bc5f1b629711395fe9704d88aed9d3839c
Jacob Ye <ye.jacob@gmail.com>
2025-07-17 23:40:59 -0400

    new problem

--- 07-17-2025/problem.py
+++ 07-17-2025/problem.py
@@ -10,4 +10,12 @@ class Solution(object):
         :type l2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        
+        dummy = ListNode()
+        current = dummy
+        carry = 0
+
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+
+            total = val1 + val2 + carry
