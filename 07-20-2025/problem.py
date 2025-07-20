c0f3563bdd4691f9610f86947cd7ab3151bfe42f
Jacob Ye <ye.jacob@gmail.com>
2025-07-20 14:18:33 -0400

    new message

--- 07-20-2025/problem.py
+++ 07-20-2025/problem.py
@@ -8,7 +8,7 @@ class Solution(object):
         merged = nums1 + nums2
         merged.sort()
         if len(merged) % 2 == 0:
-            return (merged[len(merged) // 2 - 1] + merged[len(merged) // 2]) / 2
+            return (merged[len(merged) // 2 - 1] + merged[len(merged) // 2]) / 2.0
         else:
             return merged[len(merged) // 2]
 
