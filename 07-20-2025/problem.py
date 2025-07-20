a395d0ac7763b27f3562157222252cc834ddda33
Jacob Ye <ye.jacob@gmail.com>
2025-07-20 14:15:34 -0400

    new problem

---
+++ 07-20-2025/problem.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        merged = nums1 + nums2
+        return print(merged)
+
+soltuion = Solution()
+print(soltuion.findMedianSortedArrays([1,3], [2]))
