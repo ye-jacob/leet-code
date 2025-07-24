2d47c090ec90a34536ddfe80e61684f9c0761415
Jacob Ye <ye.jacob@gmail.com>
2025-07-23 23:37:16 -0400

    sol

--- 07-23-2025/problem.py
+++ 07-23-2025/problem.py
@@ -4,28 +4,15 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        sum = 0
-        for i in range(len(s)):
-            if s[len(s)-1-i] == "I":
-                print('adding' + str(s[len(s)-1-i]))
-                sum += 1
-            if s[len(s)-1-i] == "V":
-                print('adding' + str(s[len(s)-1-i]))
-                sum += 5
-            if s[len(s)-1-i] == "X":
-                print('adding' + str(s[len(s)-1-i]))
-                sum += 10
-            if s[len(s)-1-i] == "L":
-                print('adding' + str(s[len(s)-1-i]))
-                sum += 50
-            if s[len(s)-1-i] == "C":
-                print('adding' + str(s[len(s)-1-i]))
-                sum += 100
-            if s[len(s)-1-i] == "D":
-                print('adding' + str(s[len(s)-1-i]))
-                sum += 500
-            if s[len(s)-1-i] == "M":
-                print('adding' + str(s[len(s)-1-i]))
-                sum += 1000
-        
-        return sum
+        roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        total = 0
+        prev = 0
+        # Loop from the end to the start
+        for c in reversed(s):
+            value = roman[c]
+            if value < prev:
+                total -= value
+            else:
+                total += value
+            prev = value
+        return total
