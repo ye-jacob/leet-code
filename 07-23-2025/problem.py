dc504998d43184a4f4b50a0684ebf6c324ad9613
Jacob Ye <ye.jacob@gmail.com>
2025-07-23 23:28:45 -0400

    new problem

--- 07-23-2025/problem.py
+++ 07-23-2025/problem.py
@@ -4,4 +4,28 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        sum = 0
+        for i in range(len(s)):
+            if s[len(s)-1-i] == "I":
+                print('adding' + str(s[len(s)-1-i]))
+                sum += 1
+            if s[len(s)-1-i] == "V":
+                print('adding' + str(s[len(s)-1-i]))
+                sum += 5
+            if s[len(s)-1-i] == "X":
+                print('adding' + str(s[len(s)-1-i]))
+                sum += 10
+            if s[len(s)-1-i] == "L":
+                print('adding' + str(s[len(s)-1-i]))
+                sum += 50
+            if s[len(s)-1-i] == "C":
+                print('adding' + str(s[len(s)-1-i]))
+                sum += 100
+            if s[len(s)-1-i] == "D":
+                print('adding' + str(s[len(s)-1-i]))
+                sum += 500
+            if s[len(s)-1-i] == "M":
+                print('adding' + str(s[len(s)-1-i]))
+                sum += 1000
         
+        return sum
